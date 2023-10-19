2336a97fb532e065b60fe65b3719ed4e9a653d41
Jane Calnick <janecalnick@Janes-MacBook-Pro.local>
2023-10-19 09:54:53 -0400

    new storyboard

---
+++ iOSClient/Login/ThemeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThemeViewController.swift
+//  Nextcloud
+//
+//  Created by Jane Calnick on 10/17/23.
+//  Copyright © 2023 Marino Faggiana. All rights reserved.
+//
+
+import UIKit
+
+class ThemeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
