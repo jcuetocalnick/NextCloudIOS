cda72a621c709e2a1325ac26aeb2fdd7822c36c8
Julio Padron <juliopadron@Julios-MacBook-Pro.local>
2023-10-19 23:39:51 -0400

    Theme Logo updated

---
+++ iOSClient/Login/ThemeViewController+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  ThemeViewController+Extensions.swift
+//  Nextcloud
+//
+//  Created by Julio Padron on 10/19/23.
+//  Copyright © 2023 Marino Faggiana. All rights reserved.
+//
+
+import Foundation
