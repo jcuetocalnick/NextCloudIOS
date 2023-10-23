877b6a7066192dc13d13eb39f16867c5819be63c
Julio Padron <juliopadron@Julios-MacBook-Pro.local>
2023-10-22 22:01:29 -0400

    picture logo func

--- iOSClient/Login/ThemeViewController+Extensions.swift
+++ iOSClient/Login/ThemeViewController+Extensions.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2023 Marino Faggiana. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension ThemeViewController {
+
+    /// Shows an alert with the given description or default text if nil.
+    func showAlert(description: String? = nil) {
+        let alertController = UIAlertController(title: "Oops...", message: "\(description ?? "Unknown error")", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
+}
--- iOSClient/Login/ThemeViewController.swift
+++ iOSClient/Login/ThemeViewController.swift
@@ -6,16 +6,51 @@
 //  Copyright © 2023 Marino Faggiana. All rights reserved.
 //
 
+import PhotosUI
 import UIKit
 
 class ThemeViewController: UIViewController {
 
+    @IBOutlet weak var appName: UITextField!
+    @IBOutlet weak var slogan: UITextField!
+    @IBOutlet weak var color: UIColorWell!
+    @IBOutlet weak var logo: UIImageView!
+    
+    private var pickedImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        PHPhotoLibrary.requestAuthorization { (status) in
+                    print("\(status)")
+                }
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func onTappedUploadPhoto(_ sender: UIButton) {
+        // Create and configure PHPickerViewController
+
+        // Create a configuration object
+        var config = PHPickerConfiguration()
+
+        // Set the filter to only show images as options (i.e. no videos, etc.).
+        config.filter = .images
+
+        // Request the original file format. Fastest method as it avoids transcoding.
+        config.preferredAssetRepresentationMode = .current
+
+        // Only allow 1 image to be selected at a time.
+        config.selectionLimit = 1
+
+        // Instantiate a picker, passing in the configuration.
+        let picker = PHPickerViewController(configuration: config)
+
+        // Set the picker delegate so we can receive whatever image the user picks.
+        picker.delegate = self
+
+        // Present the picker
+        present(picker, animated: true)
+    }
 
     /*
     // MARK: - Navigation
@@ -26,5 +61,47 @@ class ThemeViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension ThemeViewController: PHPickerViewControllerDelegate {
+    
+    // PHPickerViewController required delegate method.
+    // Returns PHPicker result containing picked image data.
+    
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        // Dismiss the picker
+        picker.dismiss(animated: true)
+
+        // Make sure we have a non-nil item provider
+        guard let provider = results.first?.itemProvider,
+              // Make sure the provider can load a UIImage
+              provider.canLoadObject(ofClass: UIImage.self) else { return }
+
+        // Load a UIImage from the provider
+        provider.loadObject(ofClass: UIImage.self) { [weak self] object, error in
+
+            // Make sure we can cast the returned object to a UIImage
+            guard let image = object as? UIImage else {
+                self?.showAlert()
+                return
+            }
+
+            // Check for and handle any errors
+            if let error = error {
+                self?.showAlert(description: error.localizedDescription)
+                return
+            } else {
+
+                // UI updates (like setting image on image view) should be done on main thread
+                DispatchQueue.main.async {
+
+                    // Set image on preview image view
+                    self?.logo.image = image
+
+                    // Set image to use when saving post
+                    self?.pickedImage = image
+                }
+            }
+        }
+    }
 }
